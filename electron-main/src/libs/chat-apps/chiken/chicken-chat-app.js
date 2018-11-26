455bf069e47ad99ee287b690b22ddf4e32fd6d02
Romulo <romulo.santos@king.com>
2018-11-26 17:30:38 +0100

    fixes chicken

--- electron-main/src/libs/chat-apps/chiken/chicken-chat-app.js
+++ electron-main/src/libs/chat-apps/chiken/chicken-chat-app.js
@@ -15,19 +15,19 @@ class ChickenChatApp {
     if (this.cooldownManager.isBlockedByGlobalCoolDown()) return;
     if (this.cooldownManager.isAuthorBlockedByCoolDown(message.author)) return;
 
-    let command = commands.commandInFirstWord(message.text, [this.settings.moveCommand, this.settings.sayCommand, this.settings.volunteerCommand]);
+    let command = commands.commandInFirstWord(message.text, [this.settings.options.moveCommand, this.settings.sayCommand, this.settings.volunteerCommand]);
     if (!command) return;
 
-    if (command.command === this.settings.moveCommand) {
+    if (command.command === this.settings.options.moveCommand) {
       const split = message.text.split(' ');
       if (!isNaN(split[1]) && !isNaN(split[2])) {
         const screenMessage = {chicken: {move: {x: split[1], y: split[2]}}};
         sendScreenMessage(screenMessage);
       }
     }
-    else if (command.command === this.settings.sayCommand) {
-      const theMessage = message.text.replace('!' + this.settings.sayCommand, '').replace(this.settings.sayCommand, '').substr(0, 75);
-      const screenMessage = {chicken: {say: theMessage, sound: this.settings.soundEnabled}};
+    else if (command.command === this.settings.options.sayCommand) {
+      const theMessage = message.text.replace('!' + this.settings.options.sayCommand, '').replace(this.settings.options.sayCommand, '').substr(0, 75);
+      const screenMessage = {chicken: {say: theMessage, sound: this.settings.options.soundEnabled}};
       sendScreenMessage(screenMessage);
     }
   }
