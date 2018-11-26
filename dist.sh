36675847adafba4acbf60ebfdd5233faba57a466
Romulo Santos <romulobr@gmail.com>
2018-11-26 13:07:18 +0100

    add dist.sh

--- dist.sh
+++ dist.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
+cd electron-main
+rm -rf dist
+mkdir dist
+rm -rf public-dist
+mkdir public-dist
+mkdir public-dist/scene
+cd ..
 
-bash ./prepare-dist.sh
+cd web-app
+npm install
+rm -rf build
+mkdir build
+npm run build
+cp -R ./build/* ../electron-main/public-dist
+cd ..
+
+cd scene-app
+npm install
+rm -rf build
+mkdir build
+npm run build
+cp -R ./build/* ../electron-main/public-dist/scene
+cd ..
+
+cd electron-main
+npm run dist
 
