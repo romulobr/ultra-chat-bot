f62d01a656dcb4aecde4def5ed830c11466f2325
Romulo <romulo.santos@king.com>
2019-03-04 02:48:23 +0100

    adds link to download page.

--- web-app/src/current-version/current-version.js
+++ web-app/src/current-version/current-version.js
@@ -3,6 +3,6 @@ const pjson = require('../../package.json');
 
 export default function CurrentVersion() {
     return (
-        <div>Current version is: {pjson.version}</div>
+        <div><a href="https://romulino.com/baixar" target="__blank">Current version is: {pjson.version}</a></div>
     )
 };
