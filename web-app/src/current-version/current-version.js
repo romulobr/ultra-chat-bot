319a06e83d9846bd87bfd2b59ca1011aefd7249b
Romulo <romulo.santos@king.com>
2019-03-04 22:51:24 +0100

    update current versions

--- web-app/src/current-version/current-version.js
+++ web-app/src/current-version/current-version.js
@@ -4,8 +4,9 @@ const pjson = require('../../package.json');
 
 export default function CurrentVersion() {
     return (
-        <div><a href="https://romulino.com/baixar" target="__blank">Current version is: {pjson.version}</a><br/>
-            <p>Created and Developed by romulinoTV</p>
+        <div>
+            <p><a href="https://romulino.com/baixar" target="__blank">Current version is: {pjson.version}</a></p>
+            <p>Created and Developed by <a href="https://twitch.tv/romulinotv" target="__blank">romulinoTV</a></p>
         </div>
     )
 };
