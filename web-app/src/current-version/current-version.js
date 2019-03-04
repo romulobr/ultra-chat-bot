a4581bc9b54639f8e1983b08773eb16655b2ecfb
Romulo <romulo.santos@king.com>
2019-03-04 22:16:49 +0100

    adds version number + paypal link stuff

--- web-app/src/current-version/current-version.js
+++ web-app/src/current-version/current-version.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
 const pjson = require('../../package.json');
 
 export default function CurrentVersion() {
     return (
-        <div><a href="https://romulino.com/baixar" target="__blank">Current version is: {pjson.version}</a></div>
+        <div><a href="https://romulino.com/baixar" target="__blank">Current version is: {pjson.version}</a><br/>
+            <p>Created and Developed by romulinoTV</p>
+        </div>
     )
 };
